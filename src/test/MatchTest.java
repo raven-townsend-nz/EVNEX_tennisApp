5b10f244198660c790ce06af18b0462efb1da5c7
Raven Townsend <rto45@uclive.ac.nz>
2021-08-19 19:34:39 +1200

    Added more test for Match

--- src/test/MatchTest.java
+++ src/test/MatchTest.java
@@ -56,6 +56,39 @@ public class MatchTest {
     }
 
     @Test
+    void score_playersWin6GamesEachAndDeuceOnLastGame_returnsCorrectScore() {
+        Match match = new Match("player 1", "player 2");
+        for (int i = 0; i < 6; i++) {
+            playerScores(4, match, "player 1");
+            playerScores(4, match, "player 2");
+        }
+        for (int i = 0; i < 4; i++) {
+            playerScores(1, match, "player 1");
+            playerScores(1, match, "player 2");
+        }
+        String expected = "6-6, Deuce";
+        String actual = match.score();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void score_playersWin6GamesEachAndAdvantageOnLastGAme_returnsCorrectScore() {
+        Match match = new Match("player 1", "player 2");
+        for (int i = 0; i < 6; i++) {
+            playerScores(4, match, "player 1");
+            playerScores(4, match, "player 2");
+        }
+        for (int i = 0; i < 4; i++) {
+            playerScores(1, match, "player 1");
+            playerScores(1, match, "player 2");
+        }
+        playerScores(1, match, "player 1");
+        String expected = "6-6, Advantage player 1";
+        String actual = match.score();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     void score_playersWin6GamesEachAndPlayer2WinsAnotherGame_returnsCorrectScoreAndWinner() {
         Match match = new Match("player 1", "player 2");
         for (int i = 0; i < 6; i++) {
